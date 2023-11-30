88f801e6b51c823ff925c1948028a054db7705b5
HOME-VM-TOSCHOOL <mat.dok123@gmial.com>
2023-11-30 04:30:25 +0100

    code improvements and tests

--- zaj7/regularyzacja.py
+++ zaj7/regularyzacja.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import KFold, cross_val_score, cross_validate
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 
-NUM_FOLDS = 5
+NUM_FOLDS = 7
 
 all_data = pd.read_csv(
     "communities.data",
@@ -27,10 +27,13 @@ scaled_data = pd.DataFrame(
 x_param = scaled_data[scaled_data.columns[:-1]]
 y_param = scaled_data[scaled_data.columns[-1]]
 
+# x_param = all_data[all_data.columns[:-1]]
+# y_param = all_data[all_data.columns[-1]]
+
 kf = KFold(n_splits=NUM_FOLDS, shuffle=True, random_state=42)
 
 
-model_with_ridge = make_pipeline(PolynomialFeatures(degree=2), Ridge(alpha=40.0))
+model_with_ridge = make_pipeline(PolynomialFeatures(degree=1), Ridge(alpha=3.0))
 score_ridge = -cross_val_score(
     model_with_ridge,
     x_param,
@@ -43,7 +46,7 @@ score_ridge = -cross_val_score(
 print(f"RMSE with ridge: {score_ridge.mean()} +/- {score_ridge.std()}\n")
 
 
-model_without_ridge = make_pipeline(PolynomialFeatures(degree=2), LinearRegression())
+model_without_ridge = make_pipeline(PolynomialFeatures(degree=1), LinearRegression())
 score_no_ridge = -cross_val_score(
     model_without_ridge,
     x_param,
@@ -55,9 +58,3 @@ score_no_ridge = -cross_val_score(
 
 
 print(f"RMSE without ridge: {score_no_ridge.mean()} +/- {score_no_ridge.std()}\n")
-
-
-column_index: int = 0
-unscaled = scaler.inverse_transform(scaled_data)
-assert isinstance(unscaled, np.ndarray)
-unscaled_data = pd.DataFrame(unscaled, columns=all_data.columns)
