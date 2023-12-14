b5cb38c941568b8e5d89e9e5899eadc4d4e3bba4
HOME-VM-TOSCHOOL <mat.dok123@gmail.com>
2023-12-14 21:12:18 +0100

    zaj8

--- zaj8/Uczenie nienadzorowane.py
+++ zaj8/Uczenie nienadzorowane.py
@@ -1,5 +1,12 @@
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.cluster import KMeans
+
+N_CLUSTERS = 5
 
 
 all_data = pd.read_csv(
@@ -7,6 +14,7 @@ all_data = pd.read_csv(
     sep="\t",
 )
 
+# preprocess data
 
 # change "parter" to 0
 all_data.replace("parter", "0", inplace=True)
@@ -15,3 +23,38 @@ all_data.replace(["poddasze", "niski parter"], np.nan, inplace=True)
 # drop rows with nan
 all_data.dropna(inplace=True)
 
+
+# scale and normalize data
+
+scaler = StandardScaler()
+scaled_data = pd.DataFrame(
+    data=scaler.fit_transform(all_data), columns=all_data.columns
+)
+
+
+# apply kmeans
+kmeans = KMeans(n_clusters=N_CLUSTERS)
+kmeans.fit(scaled_data)
+
+# show pairplot with coloring
+data_with_coloring = pd.DataFrame(scaled_data, columns=scaled_data.columns)
+data_with_coloring["cluster"] = kmeans.labels_
+sns.pairplot(data_with_coloring, hue="cluster", palette="viridis")
+
+
+# calculate PCA
+pca = PCA(n_components=2)
+pca_result = pca.fit_transform(scaled_data)
+
+# show PCA with K-Means Clustering Labels
+data_with_coloring = pd.DataFrame(pca_result, columns=["PC1", "PC2"])
+data_with_coloring["cluster"] = kmeans.labels_
+plt.figure(figsize=(10, 6))
+sns.scatterplot(
+    x="PC1", y="PC2", data=data_with_coloring, hue="cluster", palette="viridis", s=50
+)
+plt.title("PCA with K-Means Clustering Labels")
+plt.xlabel("Principal Component 1")
+plt.ylabel("Principal Component 2")
+
+plt.show()
